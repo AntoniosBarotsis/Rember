06044246cbc114743fbae2001b80d3cefc0b9169
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-01-06 22:50:07 +0200

    choco folder

---
+++ tools/chocolateyinstall.ps1
@@ -0,0 +1,6 @@
+﻿$name = 'Package Name'
+$installerType = 'exe'
+$url  = 'https://github.com/AntoniosBarotsis/Rember/releases/download/v0.0.1-beta/Rember.exe'
+$silentArgs = '/S'
+
+Install-ChocolateyPackage $name $installerType $silentArgs $url
