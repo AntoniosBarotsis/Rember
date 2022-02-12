ba2182effa5e24505d84de06cf9fd48cbb03fdea
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-02-12 18:52:14 +0100

    Updated configs for next version

--- RefreshPackage.ps1
+++ RefreshPackage.ps1
@@ -1,1 +1,1 @@
-dotnet pack | dotnet tool update --global --add-source .\Rember\nupkg\ rember  --version 0.0.2-beta1
+dotnet pack | dotnet tool update --global --add-source .\Rember\nupkg\ rember  --version 0.0.4-beta1
