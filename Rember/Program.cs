04fb34d95a613cbfbe2f9c7c0f7db7ca6caf8ef6
Antonios Barotsis <a.barotsis@student.tudelft.nl>
2022-05-15 21:44:22 +0200

    Updated the help page

--- Rember/Program.cs
+++ Rember/Program.cs
@@ -1,6 +1,15 @@
-﻿using CliFx;
+﻿using System.Reflection;
+using CliFx;
+
+var version = 
+    Assembly.GetEntryAssembly()!
+    .GetCustomAttribute<AssemblyInformationalVersionAttribute>()
+    ?.InformationalVersion;
 
 await new CliApplicationBuilder()
+    .SetExecutableName("rember")
+    .SetVersion($"v{version!}")
+    .SetDescription("https://github.com/AntoniosBarotsis/Rember")
     .AddCommandsFromThisAssembly()
     .Build()
     .RunAsync();
